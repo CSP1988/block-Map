4ec3d6cfa84edbfac2a91c5434d62cd769229b59
CSP1988 <411824658@qq.com>
2018-03-07 21:16:33 +0800

    增加了位置天气预报，修改部分小问题

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 
 ### 实现方式
 #### 使用Knockout 框架和Google map API
+#### 使用国内的天气预报API，可在标记详情内查看
 
 ### 下载
 #### git clone https://github.com/CSP1988/udacityProject4_-.git
--- 街区地图/src/js/app2.js
+++ 街区地图/src/js/app2.js
@@ -22,7 +22,7 @@ function initMap(){
     var centerControlDiv = document.createElement('div');
     var centerControl = new CenterControl(centerControlDiv, map);
 
-    centerControlDiv.index = 1;
+    // centerControlDiv.index = 1;
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(centerControlDiv);
 
     bounds = new google.maps.LatLngBounds();
@@ -36,7 +36,8 @@ function initMap(){
             title: itme.title
         });
         marker.addListener('click',function(){
-          openInfoWindow(marker);
+            marker.setAnimation(4);
+            openInfoWindow(marker);
         });
         markers.push(marker);
         bounds.extend(marker.position);
@@ -45,20 +46,51 @@ function initMap(){
     ko.applyBindings(new ViewModel(markers));
 }
 
-//显示标签信息
+/*
+*搜索天气
+*显示标签信息
+*/
 function openInfoWindow(marker){
-    let msg = "";
-    msg += `<div>地址：${marker.title}</div>`;
-    msg += `<div>经度：${marker.position.lat()}</div>`;
-    msg += `<div>纬度：${marker.position.lng()}</div>`
+    const APP_id = '58413';
+    const api_key = 'c07ba0154a27401f86a60a50728efec2';
 
-    infowindow.setContent(msg);
-    infowindow.open(map,marker);
-    infowindow.addListener('closeclick', function(){
-        infowindow.setMarker = null;
-    });
+    var baseUrl = `https://route.showapi.com/9-5?`
+    var queryParams = `from=5&lat=${marker.position.lat()}&lng=${marker.position.lng()}&needIndex=0&needMoreDay=0&`
+    var userInfo = `showapi_appid=${APP_id}&showapi_test_draft=false&showapi_sign=${api_key}`;
+    var url = baseUrl + queryParams + userInfo;
 
-    
+
+    //发起请求获取天气情况
+    $.ajax({
+        url: url
+    }).done(function(data, textStatus, jqXhr){
+        console.log(data);
+        var weatherData = JSON.parse(data);
+        console.dir(`weatherData = ${weatherData}`);
+
+        let msg = "";
+        msg += `<div>地址：${marker.title}</div>`;
+        msg += `<div>经度：${marker.position.lat()}</div>`;
+        msg += `<div>纬度：${marker.position.lng()}</div>`;
+        var weatherNow = weatherData.showapi_res_body.now;
+        if(weatherData.showapi_res_code == 0){
+            msg += `<div>天气：</div>`;
+            msg += `<div>${weatherNow.weather}，气温${weatherNow.temperature}度，
+                ${weatherNow.wind_direction}${weatherNow.wind_power}<image src="${weatherNow.weather_pic}"></image></div>`
+        }else{
+            msg += `...抱歉，没有获取到最新天气情况!`;
+        }
+
+        infowindow.setContent(msg);
+        infowindow.open(map,marker);
+        infowindow.addListener('closeclick', function(){
+            infowindow.setMarker = null;
+        });
+
+    }).fail(function(jqXhr, textStatus, errorThrown){
+        console.log("jqXhr:" + jqXhr +", textStatus:" + textStatus + ", errorThrown:" +errorThrown);
+    //do ajax logging on the browser console
+    });    
 }
 
 function setZoomCenter(marker){
@@ -112,29 +144,29 @@ var ViewModel = function(markers){
         self.markersSearch.dispose();
     };
     
-    function alertMarker(marker){
-        self.currentPosition(marker);
-        self.currentMarker().setAnimation(google.maps.Animation.DROP);
-        setZoomCenter(marker);
-    }
+    // function alertMarker(marker){
+    //     self.currentPosition(marker);
+    //     self.currentMarker().setAnimation(google.maps.Animation.DROP);
+    //     setZoomCenter(marker);
+    // }
     
 
-    self.goPosition = function(clickPosition){
-      console.dir(clickPosition);
-      self.currentPosition(clickPosition);
-      setZoomCenter(clickPosition);
+    self.goPosition = function(marker){
+        // console.dir(marker);
+        self.currentPosition(marker);
+        self.currentPosition().setAnimation(google.maps.Animation.DROP);
+        
+        menuSwitch();
+        setZoomCenter(marker);
+
     }
 }
 
 var mapErrorHandler = function(msg,url,l){
-  console.log('==================');
-  console.log(msg);
-  console.log(url);
-  console.log(l);
-  let text = "";
-  text = `网络连接错误\n错误信息：${msg}\n URL:${url}`
-  alert(text);
-  
+    let text = "";
+    text = `网络连接错误\n错误信息：${msg}\n URL:${url}`
+    alert(text);
+
 }
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
@@ -152,20 +184,24 @@ function closeNav() {
 
 function CenterControl(controlDiv, map) {
 
-  var controlText = document.createElement('image');
-  controlText.style.color = 'rgb(25,25,25)';
-  controlDiv.style.backgroundColor = '#fff';
-  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-  controlText.style.fontSize = '16px';
-  controlText.style.lineHeight = '38px';
-  controlText.style.paddingLeft = '5px';
-  controlText.style.paddingRight = '5px';
-  controlText.innerHTML = 'Menu';
-  controlDiv.appendChild(controlText);
+    var controlText = document.createElement('image');
+    controlText.style.color = 'rgb(25,25,25)';
+    controlDiv.style.backgroundColor = '#fff';
+    controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+    controlText.style.fontSize = '16px';
+    controlText.style.lineHeight = '38px';
+    controlText.style.paddingLeft = '5px';
+    controlText.style.paddingRight = '5px';
+    controlText.innerHTML = 'Menu';
+    controlDiv.appendChild(controlText);
 
-    //自定义按钮的点击事件
-  controlDiv.addEventListener('click', function() {
-    
+        //自定义按钮的点击事件
+    controlDiv.addEventListener('click', function() {
+        menuSwitch();
+    });
+}
+
+function menuSwitch(){
     if(!isClick){
         openNav();
         isClick = true;
@@ -173,8 +209,26 @@ function CenterControl(controlDiv, map) {
         closeNav();
         isClick = false;
     }
+}
 
-   
-  });
+//http://api.map.baidu.com/telematics/v3/movie?qt=nearby_cinema &
+//location=116.307852,40.057023 &
+//ak=E4805d16520de693a3fe707cdc962045
 
+
+//电影院标记数组
+var cinemaMarkers = [];
+
+//搜索附近天气预报
+function weatherSearch(json){
+    // cinemaMarkers.forEach(function(cinema){
+    //     cinema.setMap(null);
+    // });
+
+    
+
+    // console.dir(`url = ${url}`);
+    
+
+    
 }
